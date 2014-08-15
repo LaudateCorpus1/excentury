9cb64a4ccbeba7406424810ad860dcb7866a4546
Manuel Lopez <jmlopez.rod@gmail.com>
2014-08-15 17:37:31 -0500

    Excentury Projects
    For the most part we will be working with projects and thus we need
    to separate all the files so that they do not linger in the same
    directory. By default excentury places those files in a special
    directory. In a project however, many mistakes will be encountered.
    When this is the case we would like to see the files generated by
    excentury so that we may detect the source of the error.
    
    The `init` command creates all the directories and a special bash
    file so that the environmental paths point to the right directories.
    Notice that this file should be modified to meet the projects
    demands. For instance, a special directory where we place python
    scripts could be created and we may append it to the `PYTHONPATH`
    variable so that we do not mix the files generated by excentury and
    those created by the team.
    
    As a result of this idea the `install` command had to be modified.
    Now we have a function to generate the standard `excenturyrc` file
    which resides in `~/.excentury`. In the next commit we will create
    another command which will make use of some of the functions in the
    `install` command.

--- excentury/command/__init__.py
+++ excentury/command/__init__.py
@@ -5,7 +5,9 @@ excentury.
 
 """
 
+import os
 import sys
+import os.path as pth
 from datetime import datetime
 from subprocess import Popen, PIPE
 
@@ -19,6 +21,11 @@ def error(msg):
 def trace(msg):
     "Print a message to the standard error "
     sys.stderr.write(msg)
+
+
+def disp(msg):
+    "Print a message to the standard output "
+    sys.stdout.write(msg)
 
 
 def import_mod(name):
@@ -48,3 +55,18 @@ def date(short=False):
     if not short:
         return now.strftime("%a %b %d, %Y %r")
     return now.strftime("%Y-%m-%d-%H-%M-%S")
+
+
+def make_dir(path):
+    "Create a directory if it does not exist and return True. "
+    if pth.exists(path):
+        return False
+    os.makedirs(path)
+    return True
+
+
+def append_variable(var, val):
+    """Return a valid bash string to append a value to a variable. """
+    ans = 'if [[ ":$%s:" != *":%s:"* ]]; then\n' % (var, val)
+    ans += '    export %s=%s:$%s\n' % (var, val, var)
+    return ans + 'fi\n'
