bc64ba9ef5590796e610ad00053c3c984d8ba7de
Manuel Lopez <jmlopez.rod@gmail.com>
2014-08-16 17:24:30 -0500

    Unit Testing
    A new xcpp file has been created so that we may test the
    interoperability between C++ and the other languages. The tests are
    very simple and they verify that excentury is working properly.
    
    It should be noted that the test only work if we are using the
    project. That is, the `xcpp.config` file will not help us much if we
    do not source the `.xcpprc` file. Make sure to select the `xc-tests`
    project before trying to run make.

---
+++ tests/cpp/xc_test-cube_mex.cpp
@@ -0,0 +1,24 @@
+// File generated on Sat Aug 16, 2014 03:45:54 PM by xcpp.
+#define XC_MATLAB
+#include <excentury/excentury.h>
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[])
+{
+    size_t ncin_ = mxGetScalar(prhs[0]);
+    char* pcin_ = mxArrayToString(prhs[1]);
+    excentury::STextInterface<excentury::load_mode> XC_LI_(pcin_, ncin_);
+    int x(3); XC_LI_.load(x);
+    XC_LI_.close();
+
+    int result = x*x*x;
+
+    excentury::STextInterface<excentury::dump_mode> XC_DI_;
+    XC_DI_.dump(result, "result");
+    XC_DI_.close();
+    std::string xc_mex_str_ = XC_DI_.str();
+    plhs[0] = mxCreateDoubleMatrix(1, 1, mxREAL);
+    double* ncout_ = mxGetPr(plhs[0]);
+    ncout_[0] = xc_mex_str_.size();
+    plhs[1] = mxCreateString(xc_mex_str_.data());
+    mxFree(pcin_);
+}
